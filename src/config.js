c316a07833d4ffa42109c0595067d784a2781b51
joaogabrielmp <joaogabrielma@gmail.com>
2018-06-27 20:17:04 -0300

    Up

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
+
+module.exports = {
+    connectionString: 'mongodb://jgmp:jgmp@ds062448.mlab.com:62448/node-test',
+    sendgridKey: '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',
+    containerConnectionString: 'Azure'
+}
